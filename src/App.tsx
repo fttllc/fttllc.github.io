9abd15f8a0955bc18ed8dc92179e1484ec06e05e
deckwill <deck.william@gmail.com>
2021-09-04 12:19:15 -0500

    set site under contruction

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fly in the ointment.
+          Site is under construction. Please check back soon.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
