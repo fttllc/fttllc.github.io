b2faa6dfccff7e90eac0db83ae3a82fa13021699
deckwill <deck.william@gmail.com>
2021-09-08 09:09:05 -0500

    add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <p className="logo">FTT, LLC</p>
-      </header>
-      <body className="App-body">
+      <AppBar className="App-header" position="static">
+        <Toolbar className="App-header">
+          <Button className="menuButton" color="inherit">  
+            <Typography variant="h4" className="logo" align="left">
+                FTT, LLC
+            </Typography>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <div className="App-body">
         <div>
           <p>
               Site is under construction. Please check back soon.
           </p>
         </div>
-      </body>
+      </div>
     </div>
   );
 }
